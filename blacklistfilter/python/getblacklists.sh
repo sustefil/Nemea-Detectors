0505cf08ef8dcd2720ba18b3a41d3b61ad79eb53
Filip Šuster <sustefil@fit.cvut.cz>
2018-01-31 12:40:49 +0100

    Add ransomware BL

--- blacklistfilter/python/getblacklists.sh
+++ blacklistfilter/python/getblacklists.sh
@@ -4,3 +4,5 @@ curl https://www.spamhaus.org/drop/edrop.txt | sed 's/\s*;.*$//;' > spamhaus-org
 
 curl https://lists.blocklist.de/lists/ssh.txt > blocklist-de-ssh.txt
 
+curl https://ransomwaretracker.abuse.ch/downloads/RW_IPBL.txt |grep -v '^#' > abuse-ch-ransomware.txt
+
