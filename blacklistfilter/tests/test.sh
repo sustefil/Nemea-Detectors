62b002d999c87613fca85a56b057da7f6caa80ce
Filip Suster <sustefil@fit.cvut.cz>
2018-10-26 13:31:39 +0200

    blacklistfilter: tests: IPV6 tests

--- blacklistfilter/tests/test.sh
+++ blacklistfilter/tests/test.sh
@@ -8,48 +8,109 @@ LOGREPLAY_BIN="/usr/local/bin/logreplay"
 BLACKLIST_FOLDER="test_blacklists"
 
 IPFILTER_CONFIG="ipdetect_config_test.xml"
-REF_OUTPUT_FOLDER="ref_output"
-OUTPUT_FOLDER="output"
 
-rm -r $OUTPUT_FOLDER 2>/dev/null
-mkdir $OUTPUT_FOLDER
+IPV4_REF_OUTPUT_FOLDER="ref_output4"
+IPV6_REF_OUTPUT_FOLDER="ref_output6"
+IPV4_OUTPUT_FOLDER="output4"
+IPV6_OUTPUT_FOLDER="output6"
 
-TEST_COUNT=6
-MAIN_RET=0
-for ((i=1; i<=$TEST_COUNT;i++)); do
-    # Replace the blacklist in the config file
-    sed -i "s/test_blacklists\/ip.\.blist/test_blacklists\/ip$i.blist/" $IPFILTER_CONFIG
+rm -r $IPV4_OUTPUT_FOLDER $IPV6_OUTPUT_FOLDER 2>/dev/null
+mkdir $IPV4_OUTPUT_FOLDER $IPV6_OUTPUT_FOLDER
 
-    # Run blacklistfilter
-    $IPFILTER_BIN -i u:blacklist_test,f:out:w -u $IPFILTER_CONFIG &
-    sleep 1
 
-    # The first two testcases use testfile_20
-    if [[ $i < 3 ]]; then
-        $LOGREPLAY_BIN -f testfile_20 -i u:blacklist_test
+function test4
+{
+    sed -i "s/test_blacklists\/ip4_dummy.blist/test_blacklists\/ip4_1.blist/" $IPFILTER_CONFIG
+
+    TEST_COUNT=6
+    MAIN_RET=0
+    for ((i=1; i<=$TEST_COUNT;i++)); do
+        # Replace the blacklist in the config file
+        sed -i "s/test_blacklists\/ip4_.\.blist/test_blacklists\/ip4_$i.blist/" $IPFILTER_CONFIG
+        # Suppress ipv6 for ipv4 tests
+        sed -i "s/test_blacklists\/ip6_.\.blist/test_blacklists\/ip6_dummy.blist/" $IPFILTER_CONFIG
+
+        # Run blacklistfilter
+        $IPFILTER_BIN -i u:blacklist_test,f:out:w -u $IPFILTER_CONFIG &
+        sleep 1
+
+        # The first two testcases use testfile_20
+        if [[ $i < 3 ]]; then
+            $LOGREPLAY_BIN -f testfile_20 -i u:blacklist_test
+        else
+            $LOGREPLAY_BIN -f testfile_1000 -i u:blacklist_test
+        fi
+
+        sleep 1
+        $LOGGER_BIN -t -i f:out > $IPV4_OUTPUT_FOLDER/$i.out
+        cmp $IPV4_OUTPUT_FOLDER/$i.out $IPV4_REF_OUTPUT_FOLDER/$i.out
+        RET=$?
+
+        if [[ $RET -eq 0 ]]; then
+            echo "Test $i/$TEST_COUNT: SUCCESS"
+        else
+            echo "Test $i/$TEST_COUNT: FAIL"
+            MAIN_RET=1
+        fi
+    done
+
+    rm out
+    # On success, delete the output folder
+    if [[ $MAIN_RET -eq 0 ]]; then
+        rm -r $IPV4_OUTPUT_FOLDER
     else
+        echo "Keeping output folder to check for errors"
+    fi
+
+    return $MAIN_RET
+}
+
+function test6
+{
+    sed -i "s/test_blacklists\/ip6_dummy.blist/test_blacklists\/ip6_1.blist/" $IPFILTER_CONFIG
+
+    TEST_COUNT=2
+    MAIN_RET=0
+    for ((i=1; i<=$TEST_COUNT;i++)); do
+        # Replace the blacklist in the config file
+        sed -i "s/test_blacklists\/ip4_.\.blist/test_blacklists\/ip4_dummy.blist/" $IPFILTER_CONFIG
+        # Suppress ipv6 for ipv4 tests
+        sed -i "s/test_blacklists\/ip6_.\.blist/test_blacklists\/ip6_$i.blist/" $IPFILTER_CONFIG
+
+        # Run blacklistfilter
+        $IPFILTER_BIN -i u:blacklist_test,f:out:w -u $IPFILTER_CONFIG &
+        sleep 1
+
         $LOGREPLAY_BIN -f testfile_1000 -i u:blacklist_test
-    fi
 
-    sleep 1
-    $LOGGER_BIN -t -i f:out > $OUTPUT_FOLDER/$i.out
-    cmp $OUTPUT_FOLDER/$i.out $REF_OUTPUT_FOLDER/$i.out
-    RET=$?
+        sleep 1
+        $LOGGER_BIN -t -i f:out > $IPV6_OUTPUT_FOLDER/$i.out
+        cmp $IPV6_OUTPUT_FOLDER/$i.out $IPV6_REF_OUTPUT_FOLDER/$i.out
+        RET=$?
 
-    if [[ $RET -eq 0 ]]; then
-        echo "Test $i/$TEST_COUNT: SUCCESS"
+        if [[ $RET -eq 0 ]]; then
+            echo "Test $i/$TEST_COUNT: SUCCESS"
+        else
+            echo "Test $i/$TEST_COUNT: FAIL"
+            MAIN_RET=1
+        fi
+    done
+
+    rm out
+    # On success, delete the output folder
+    if [[ $MAIN_RET -eq 0 ]]; then
+        rm -r $IPV6_OUTPUT_FOLDER
     else
-        echo "Test $i/$TEST_COUNT: FAIL"
-        MAIN_RET=1
+        echo "Keeping output folder to check for errors"
     fi
-done
 
-rm out
-# On success, delete the output folder
-if [[ $MAIN_RET -eq 0 ]]; then
-    rm -r $OUTPUT_FOLDER
-else
-    echo "Keeping output folder to check for errors"
-fi
+    return $MAIN_RET
+}
 
-exit $MAIN_RET
+# test4
+RET4=$?
+
+test6
+RET6=$?
+
+exit $(( $RET4 || $RET6 ))
